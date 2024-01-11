b2c909c686442cbe4912650a1eb8002e8b13bf65
Anastacia <anaanestesia@gmail.com>
2024-01-11 12:44:00 +0000

    adding client attributes

--- includes/Client.hpp
+++ includes/Client.hpp
@@ -8,15 +8,20 @@ class Client {
 	private:
 		struct sockaddr_storage	addr;
 		socklen_t				size;
+		std::string				username;
+		std::string				nickname;
 
 	public:
 		Client(struct sockaddr_storage addr, socklen_t size);
 		~Client();
 		Client(Client const & src);
 		Client & operator=(Client const & rhs);
+		std::string getUsername() const;
+		std::string getNickname() const;
+		void setUsername(std::string name);
+		void setNickname(std::string name);
 		sockaddr_storage getAddr() const;
 		socklen_t getSize() const;
-		void setSize(socklen_t size);
 		static Client * createClient(struct sockaddr_storage addr, socklen_t size);
 
 
--- srcs/Client.cpp
+++ srcs/Client.cpp
@@ -12,9 +12,26 @@ Client::Client(Client const & src) {
 Client & Client::operator=(Client const & rhs) {
 	this->addr = rhs.getAddr();
 	this->size = rhs.getSize();
+	this->username = rhs.getUsername();
+	this->nickname = rhs.getNickname();
 	return *this;
 }
 
+std::string Client::getUsername() const {
+	return username;
+}
+
+std::string Client::getNickname() const {
+	return nickname;
+}
+
+void Client::setUsername(std::string name) {
+	this->username = name;
+}
+
+void Client::setNickname(std::string name) {
+	this->nickname = name;
+}
 
 sockaddr_storage Client::getAddr() const {
 	return addr;
@@ -24,9 +41,6 @@ socklen_t Client::getSize() const {
 	return size;
 }
 
-void Client::setSize(socklen_t size) {
-	this->size = size;
-}
 
 Client * Client::createClient(struct sockaddr_storage addr, socklen_t size) {
 	return new Client(addr, size);
