12cb31404ebece2e250f40f634eded2bd0c01991
Anastacia Silva canto <ansilva-@c1r20s4.42lisboa.com>
2024-01-18 16:27:03 +0000

    including error message on main

--- srcs/ftIrc.cpp
+++ srcs/ftIrc.cpp
@@ -8,6 +8,8 @@ int main(int ac, char** av) {
 		Server server = Server(av[1], av[2]);
 
 		server.initialize_server();
+	} else {
+		std::cout << "Usage: ./ircserv <port> <password>" << std::endl;
 	}
 
 	return 0;
