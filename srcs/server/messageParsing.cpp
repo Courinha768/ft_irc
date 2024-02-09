92f4df6bfd4c79650c8b78f1b27bc5e2aae71b94
Courinha <antoniocourinha768@gmail.com>
2024-02-09 13:22:45 +0000

    changed Server to server

--- srcs/server/messageParsing.cpp
+++ srcs/server/messageParsing.cpp
@@ -43,8 +43,18 @@ static void commandUSER(Client & client, Server & server)	{
 
 static void commandJOIN(Client & client, Server & server)	{
 
-	(void)client;
 	(void)server;
+	std::string hello = "hexchatClient!Hexchat@127.0.0.1 JOIN #hello\r\n";
+	send(client.getFd(), hello.c_str(), hello.size(), MSG_NOSIGNAL);
+
+	// std::string topic = RPL_TOPIC(client.getNickname(), "hello", "YES");
+	// send(client.getFd(), topic.c_str(), topic.size(), MSG_NOSIGNAL);
+
+	std::string namreply = RPL_NAMREPLY(client.getNickname(), "hello");
+	send(client.getFd(), namreply.c_str(), namreply.size(), MSG_NOSIGNAL);
+
+	std::string endofname = RPL_ENDOFNAMES(client.getNickname(), "hello");
+	send(client.getFd(), endofname.c_str(), endofname.size(), MSG_NOSIGNAL);
 
 }
 
