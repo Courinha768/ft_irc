8968cffeabc44352f60077e0fda65a9c0077220e
Anastacia Silva canto <ansilva-@c2r7s6.42lisboa.com>
2024-02-24 11:29:38 +0000

    solving quit problems

--- srcs/commands/quit.cpp
+++ srcs/commands/quit.cpp
@@ -7,26 +7,29 @@ void Server::commandQUIT(Client & client)	{
 		reason = message.substr(pos + 1);
 	}
 	client.setStatus(false);
+	// this print on server side can be ommited when project is ready
+	std::cout << "connection lost with client " << client.getTextAddr() << "\n";
+
+	std::vector<std::vector<Channel>::iterator> list_of_empty_channels;
 
 	std::string msg = ":" + client.getNickname() + " " + message; 
 	std::vector<Channel>::iterator it = channels.begin();
 	while (it != channels.end()) {
+		sendMessageToClient(msg, client.getFd());
 		(*it).removeClient(client);
 		if ((*it).getClients().empty()) {
-			channels.erase(it);
+			list_of_empty_channels.push_back(it);
 		} else {
 			for (size_t j = 0; j < (*it).getClients().size(); j++) {
 				sendMessageToClient(msg, (*it).getClients().at(j).getFd());
 			}
 		}
-		
 		it++;
 	}
 
-	// this print on server side can be ommited when project is ready
-	std::cout << "connection lost with client " << client.getTextAddr() << "\n";
+	for (size_t i = 0; i < list_of_empty_channels.size(); i++) {
+		channels.erase(list_of_empty_channels.at(i));
+	}
 
-	std::string quit = "Quit: " + reason;
-	sendMsg(client, quit);
 
 }
